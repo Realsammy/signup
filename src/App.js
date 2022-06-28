597d31784b0154b3736241d44aaabaf86d9ad7f3
Realsammy <realsammy86@gmail.com>
2022-06-28 15:53:45 +0100

    Committing project files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import React from 'react';
+import Sign from './Components/Sign';
+
+
+ function App() {
+  return (
+    <div className="App">
+
+        <Sign />
+
+    </div>
+  );
+}
+
+export default App;
